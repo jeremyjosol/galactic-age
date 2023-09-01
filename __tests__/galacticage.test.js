1de685cd6e49b69a70eba821d727990828e1b006
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-01 09:24:21 -0700

    Write next test

--- __tests__/galacticage.test.js
+++ __tests__/galacticage.test.js
@@ -18,9 +18,15 @@ describe('Super Galactic Age Calculator', () => {
     const mercuryAge = humanInput.mercuryYears(); 
     expect(mercuryAge).toEqual(6.4799999999999995);
 });
-//test 3
+//test 3 pass
   test('it should convert earthAge when the venusYears function is called and return the correct number in venusAge', () => {
     const venusAge = humanInput.venusYears();
     expect(venusAge).toEqual(16.74);
 });
+//test 4
+  test('it should convert earthAge when the marsYears function is called and return the correct number in marsAge', () => {
+    const marsAge = humanInput.marsYears();
+    expect(marsAge).toEqual(50.76);
+});
+
 });
