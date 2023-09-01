68c0c54b21cb22bf27b802d7287f02e1464b3801
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-01 09:18:19 -0700

    Write next test

--- __tests__/galacticage.test.js
+++ __tests__/galacticage.test.js
@@ -3,31 +3,25 @@ import GalacticAgeCalculator from './../src/galacticage.js';
 
 describe('Super Galactic Age Calculator', () => {
   let humanInput;
-
-// pass
+//test 1 pass
   test('it should return the correct number passed into the parameter of a humanInput object', () => {
     const humanInput = new GalacticAgeCalculator(27); 
     expect(humanInput.earthAge).toEqual(27);
 });
-// pass
 
   beforeEach(() => {
     const earthAge = 27;
     humanInput = new GalacticAgeCalculator(earthAge);
-    
-
 });
-
+//test 2 pass
   test('it should convert earthAge when the mercuryYears function is called and return the correct number in mercuryAge', () => {
-    // const earthAge = 27;
-    // const humanInput = new GalacticAgeCalculator(earthAge);
     const mercuryAge = humanInput.mercuryYears(); 
     expect(mercuryAge).toEqual(6.4799999999999995);
 });
-
-//   test('it should convert earthAge when the venusYears function is called and return the correct number in venusAge', () => {
-//     const earthAge = 27;
-
-// });
+//test 3
+  test('it should convert earthAge when the venusYears function is called and return the correct number in venusAge', () => {
+    const venusAge = humanInput.venusYears();
+    expect(venusAge).toEqual(16.74);
+});
 
 });
