f5d4db51f594d64daa9f42483cd8e77462ecafa8
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-01 08:29:42 -0700

    Write first test

--- __tests__/galacticage.test.js
+++ __tests__/galacticage.test.js
@@ -1,4 +1,12 @@
 // import Constructor from './../src/javascript.js';
+// Super Galactic Age Calculator
 
-// describe('Constructor', () => {}
-// test() => {}
+
+describe('Super Galactic Age Calculator', () => {
+
+  test('it should return the correct number passed into the parameter of a humanInput object', () => {
+    let humanInput = new GalacticAgeCalculator(27) 
+    expect(humanInput.age).toEqual(27);
+
+});
+});
