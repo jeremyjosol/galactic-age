9a02aeb2b603a0a66eaf0250902c299e8f3c355f
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-01 11:35:50 -0700

    Write next test

--- __tests__/galacticage.test.js
+++ __tests__/galacticage.test.js
@@ -44,7 +44,7 @@ describe('Super Galactic Passed Birthdays', () => {
   expect(ageDifference).toEqual(17);
 });
 //test 7 pass
-  test('it should return the difference of two parameters and multiply it by mercuryYears', () => {
+  test('it should return the difference of two parameters and divide it by mercuryYears', () => {
   const ageDifference = earthAge - passedEarthAge;
   let humanInput = new GalacticAgeCalculator(ageDifference);
   const passedBirthday = humanInput.mercuryYears();
@@ -58,9 +58,12 @@ describe('Super Galactic Passed Birthdays', () => {
 });
 
   beforeEach(() => {
-    const earthAge = 27;
-    const PassedEarthAge = 10;
     humanInput = new GalacticAgeCalculator(earthAge, passedEarthAge);
+});
+//test 9
+  test('it should return the correct amount of mercuryYearsPassed when called', () => {
+    const mercuryYearsPassed = humanInput.mercuryYearsPassed();
+    expect(mercuryYearsPassed).toEqual(70.83333333333334);
 });
 
 });
